2b21294bb973c6549d8b9385bbe5753c8fe8f9a3
Gabriel Louzada da Silva <131915775+Louzada27@users.noreply.github.com>
2026-03-22 12:32:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// -------- STRUCTS --------
 typedef struct {
     double receitaBruta;
     double deducoes;
@@ -12,150 +14,168 @@ typedef struct {
 } DRE;
 
 typedef struct {
-    // Ativo
+    // Ativo Circulante
     double caixa, bancos, aplicacoes, contasReceber, estoques;
+
+    // Ativo Não Circulante
     double realizavelLP, investimentos, imobilizado, intangivel;
+
     // Passivo
     double fornecedores, contasPagar;
     double emprestimosLP, provisoes;
-    // Patrimonio Liquido
+
+    // PL
     double capitalSocial, reservasCapital, reservasLucros, lucrosAcumulados;
 } Balanco;
 
 typedef struct {
-    // Resultados
+    double receitaLiquida;
     double lucroBruto;
     double lucroOperacional;
     double lucroAntesIR;
     double lucroLiquido;
 } Resultado;
 
+// -------- DRE --------
 void calcularDRE(DRE *dre, Resultado *res) {
-    double receitaLiquida = dre->receitaBruta - dre->deducoes;
-    res->lucroBruto = receitaLiquida - dre->custoMercadorias;
+
+    res->receitaLiquida = dre->receitaBruta - dre->deducoes;
+    res->lucroBruto = res->receitaLiquida - dre->custoMercadorias;
     res->lucroOperacional = res->lucroBruto - dre->despesasOperacionais;
     res->lucroAntesIR = res->lucroOperacional - dre->despesasFinanceiras + dre->receitasFinanceiras;
-    res->lucroLiquido = res->lucroAntesIR - dre->impostoRenda - dre->csl;
+    res->lucroLiquido = res->lucroAntesIR - (dre->impostoRenda + dre->csl);
 
-    printf("\n--- DRE ---\n");
-    printf("Receita Liquida:\tR$ %.2f\n", receitaLiquida);
-    printf("Lucro Bruto:\t\tR$ %.2f\n", res->lucroBruto);
-    printf("Lucro Operacional:\tR$ %.2f\n", res->lucroOperacional);
-    printf("Lucro Antes IR:\t\tR$ %.2f\n", res->lucroAntesIR);
-    printf("Lucro Liquido:\t\tR$ %.2f\n", res->lucroLiquido);
+    printf("\n--- RELATORIO: DRE ---\n");
+    printf("Receita Liquida:\t\t R$ %.2f\n", res->receitaLiquida);
+    printf("Lucro Bruto:\t\t\t R$ %.2f\n", res->lucroBruto);
+    printf("Lucro Operacional:\t\t R$ %.2f\n", res->lucroOperacional);
+    printf("Lucro Antes IR:\t\t\t R$ %.2f\n", res->lucroAntesIR);
+    printf("Lucro Liquido:\t\t\t R$ %.2f\n", res->lucroLiquido);
 }
 
-void calcularBalanco(Balanco *balanco, Resultado *res) {
-    double totalAtivo = balanco->caixa + balanco->bancos + balanco->aplicacoes + balanco->contasReceber + balanco->estoques
-                       + balanco->realizavelLP + balanco->investimentos + balanco->imobilizado + balanco->intangivel;
-    
-    double totalPassivo = balanco->fornecedores + balanco->contasPagar + balanco->emprestimosLP + balanco->provisoes;
-    
-    double totalPL = balanco->capitalSocial + balanco->reservasCapital + balanco->reservasLucros + balanco->lucrosAcumulados + res->lucroLiquido;
+// -------- BALANÇO --------
+void calcularBalanco(Balanco *b, double lucro) {
+
+    b->lucrosAcumulados += lucro;
+
+    double ac = b->caixa + b->bancos + b->aplicacoes + b->contasReceber + b->estoques;
+    double anc = b->realizavelLP + b->investimentos + b->imobilizado + b->intangivel;
+    double ativoTotal = ac + anc;
+
+    double pc = b->fornecedores + b->contasPagar;
+    double pnc = b->emprestimosLP + b->provisoes;
+    double pl = b->capitalSocial + b->reservasCapital + b->reservasLucros + b->lucrosAcumulados;
+
+    double total = pc + pnc + pl;
 
     printf("\n--- BALANCO PATRIMONIAL ---\n");
-    printf("Total Ativo:\t\tR$ %.2f\n", totalAtivo);
-    printf("Total Passivo:\t\tR$ %.2f\n", totalPassivo);
-    printf("Total Patrimonio Liquido:\tR$ %.2f\n", totalPL);
 
-    if(totalAtivo == totalPassivo + totalPL)
-        printf("Equacao Fundamental: OK (Ativo = Passivo + PL)\n");
+    printf("\nATIVO\n");
+    printf("Circulante:\t\t R$ %.2f\n", ac);
+    printf("Nao Circulante:\t\t R$ %.2f\n", anc);
+    printf("TOTAL:\t\t\t R$ %.2f\n", ativoTotal);
+
+    printf("\nPASSIVO\n");
+    printf("Circulante:\t\t R$ %.2f\n", pc);
+    printf("Nao Circulante:\t\t R$ %.2f\n", pnc);
+    printf("TOTAL:\t\t\t R$ %.2f\n", pc + pnc);
+
+    printf("\nPATRIMONIO LIQUIDO\n");
+    printf("TOTAL:\t\t\t R$ %.2f\n", pl);
+
+    printf("\nPASSIVO + PL:\t\t R$ %.2f\n", total);
+
+    if (ativoTotal == total)
+        printf("EQUAÇÃO FUNDAMENTAL: OK\n");
     else
-        printf("Equacao Fundamental: NAO RESPEITADA!\n");
+        printf("EQUAÇÃO FUNDAMENTAL: ERRO\n");
 }
 
-void calcularIndices(Balanco *balanco, DRE *dre, Resultado *res) {
-    printf("\n--- INDICES FINANCEIROS ---\n");
+// -------- INDICES --------
+void calcularIndices(Balanco *b, DRE *d, Resultado *r) {
 
-    // Liquidez
-    double liqCorrente = (balanco->caixa + balanco->bancos + balanco->aplicacoes + balanco->contasReceber + balanco->estoques)
-                        / (balanco->fornecedores + balanco->contasPagar);
-    double liqSeca = (balanco->caixa + balanco->bancos + balanco->aplicacoes + balanco->contasReceber)
-                     / (balanco->fornecedores + balanco->contasPagar);
-    printf("Liquidez Corrente:\t%.2f\n", liqCorrente);
-    printf("Liquidez Seca:\t\t%.2f\n", liqSeca);
+    double ac = b->caixa + b->bancos + b->aplicacoes + b->contasReceber + b->estoques;
+    double pc = b->fornecedores + b->contasPagar;
+    double ativoTotal = ac + b->realizavelLP + b->investimentos + b->imobilizado + b->intangivel;
+    double passivoTotal = pc + b->emprestimosLP + b->provisoes;
 
-    // Giro Estoque
-    double giroEstoque = dre->custoMercadorias / balanco->estoques;
-    printf("Giro do Estoque:\t%.2f\n", giroEstoque);
+    double compras = d->custoMercadorias * 0.7;
 
-    // Prazo Medio Recebimento
-    double vendasAnuais = dre->receitaBruta;
-    double prazoReceb = balanco->contasReceber / (vendasAnuais / 365);
-    printf("Prazo Medio Recebimento:\t%.2f dias\n", prazoReceb);
+    printf("\n--- INDICES ---\n");
 
-    // Prazo Medio Pagamento
-    double comprasAnuais = dre->custoMercadorias; // supondo 100% CMV
-    double prazoPagamento = (balanco->fornecedores) / (comprasAnuais / 365);
-    printf("Prazo Medio Pagamento:\t%.2f dias\n", prazoPagamento);
+    printf("Liquidez Corrente:\t %.2f\n", ac / pc);
+    printf("Liquidez Seca:\t\t %.2f\n", (ac - b->estoques) / pc);
 
-    // Giro do Ativo Total
-    double totalAtivo = balanco->caixa + balanco->bancos + balanco->aplicacoes + balanco->contasReceber + balanco->estoques
-                       + balanco->realizavelLP + balanco->investimentos + balanco->imobilizado + balanco->intangivel;
-    double giroAtivo = vendasAnuais / totalAtivo;
-    printf("Giro do Ativo Total:\t%.2f\n", giroAtivo);
+    printf("Giro Estoque:\t\t %.2f\n", d->custoMercadorias / b->estoques);
+    printf("PM Recebimento:\t\t %.2f dias\n", b->contasReceber / (d->receitaBruta / 365));
+    printf("PM Pagamento:\t\t %.2f dias\n", b->fornecedores / (compras / 365));
 
-    // Endividamento
-    double totalPassivo = balanco->fornecedores + balanco->contasPagar + balanco->emprestimosLP + balanco->provisoes;
-    double endividamento = totalPassivo / totalAtivo;
-    printf("Endividamento Geral:\t%.2f\n", endividamento);
+    printf("Giro Ativo:\t\t %.2f\n", d->receitaBruta / ativoTotal);
+    printf("Endividamento:\t\t %.2f\n", passivoTotal / ativoTotal);
 
-    // Rentabilidade
-    printf("Margem Lucro Bruto:\t%.2f\n", res->lucroBruto / vendasAnuais);
-    printf("Margem Lucro Operacional:\t%.2f\n", res->lucroOperacional / vendasAnuais);
-    printf("Margem Lucro Liquido:\t%.2f\n", res->lucroLiquido / vendasAnuais);
+    printf("Margem Bruta:\t\t %.2f%%\n", (r->lucroBruto / r->receitaLiquida) * 100);
+    printf("Margem Operacional:\t %.2f%%\n", (r->lucroOperacional / r->receitaLiquida) * 100);
+    printf("Margem Liquida:\t\t %.2f%%\n", (r->lucroLiquido / r->receitaLiquida) * 100);
 }
 
+// -------- MAIN --------
 int main() {
-    DRE dre;
-    Balanco balanco;
-    Resultado res;
 
-    printf("---- DRE ----\n");
-    printf("Receita Bruta: ");
-    scanf("%lf", &dre.receitaBruta);
-    printf("Deducoes: ");
-    scanf("%lf", &dre.deducoes);
-    printf("Custo Mercadorias (CMV): ");
-    scanf("%lf", &dre.custoMercadorias);
-    printf("Despesas Operacionais: ");
-    scanf("%lf", &dre.despesasOperacionais);
-    printf("Despesas Financeiras: ");
-    scanf("%lf", &dre.despesasFinanceiras);
-    printf("Receitas Financeiras: ");
-    scanf("%lf", &dre.receitasFinanceiras);
-    printf("Imposto de Renda: ");
-    scanf("%lf", &dre.impostoRenda);
-    printf("CSL: ");
-    scanf("%lf", &dre.csl);
+    DRE d = {0};
+    Balanco b = {0};
+    Resultado r = {0};
+    char nome[50];
 
-    calcularDRE(&dre, &res);
+    printf("Nome da Empresa: ");
+    scanf(" %[^\n]", nome); // CORREÇÃO AQUI
 
-    printf("\n---- BALANCO PATRIMONIAL ----\n");
+    // -------- DRE --------
+    printf("\n--- ENTRADA DRE ---\n");
+    printf("Receita Bruta: "); scanf("%lf", &d.receitaBruta);
+    printf("Deducoes: "); scanf("%lf", &d.deducoes);
+    printf("CMV: "); scanf("%lf", &d.custoMercadorias);
+    printf("Despesas Operacionais: "); scanf("%lf", &d.despesasOperacionais);
+    printf("Despesas Financeiras: "); scanf("%lf", &d.despesasFinanceiras);
+    printf("Receitas Financeiras: "); scanf("%lf", &d.receitasFinanceiras);
+    printf("IR: "); scanf("%lf", &d.impostoRenda);
+    printf("CSL: "); scanf("%lf", &d.csl);
 
-    printf("Caixa: "); scanf("%lf", &balanco.caixa);
-    printf("Bancos: "); scanf("%lf", &balanco.bancos);
-    printf("Aplicacoes Financeiras: "); scanf("%lf", &balanco.aplicacoes);
-    printf("Contas a Receber: "); scanf("%lf", &balanco.contasReceber);
-    printf("Estoques: "); scanf("%lf", &balanco.estoques);
+    calcularDRE(&d, &r);
 
-    printf("Realizavel a Longo Prazo: "); scanf("%lf", &balanco.realizavelLP);
-    printf("Investimentos: "); scanf("%lf", &balanco.investimentos);
-    printf("Imobilizado: "); scanf("%lf", &balanco.imobilizado);
-    printf("Intangivel: "); scanf("%lf", &balanco.intangivel);
+    // -------- ATIVO CIRCULANTE --------
+    printf("\n--- ENTRADA BALANCO (ATIVO CIRCULANTE) ---\n");
+    printf("Caixa: "); scanf("%lf", &b.caixa);
+    printf("Bancos: "); scanf("%lf", &b.bancos);
+    printf("Aplicacoes: "); scanf("%lf", &b.aplicacoes);
+    printf("Contas a Receber: "); scanf("%lf", &b.contasReceber);
+    printf("Estoques: "); scanf("%lf", &b.estoques);
 
-    printf("Fornecedores: "); scanf("%lf", &balanco.fornecedores);
-    printf("Contas a Pagar: "); scanf("%lf", &balanco.contasPagar);
-    printf("Emprestimos e Financiamentos: "); scanf("%lf", &balanco.emprestimosLP);
-    printf("Provisoes: "); scanf("%lf", &balanco.provisoes);
+    // -------- ATIVO NÃO CIRCULANTE --------
+    printf("\n--- ENTRADA BALANCO (ATIVO NAO CIRCULANTE) ---\n");
+    printf("Realizavel LP: "); scanf("%lf", &b.realizavelLP);
+    printf("Investimentos: "); scanf("%lf", &b.investimentos);
+    printf("Imobilizado: "); scanf("%lf", &b.imobilizado);
+    printf("Intangivel: "); scanf("%lf", &b.intangivel);
 
-    printf("Capital Social: "); scanf("%lf", &balanco.capitalSocial);
-    printf("Reservas de Capital: "); scanf("%lf", &balanco.reservasCapital);
-    printf("Reservas de Lucros: "); scanf("%lf", &balanco.reservasLucros);
-    printf("Lucros Acumulados: "); scanf("%lf", &balanco.lucrosAcumulados);
+    // -------- PASSIVO CIRCULANTE --------
+    printf("\n--- ENTRADA BALANCO (PASSIVO CIRCULANTE) ---\n");
+    printf("Fornecedores: "); scanf("%lf", &b.fornecedores);
+    printf("Contas a Pagar: "); scanf("%lf", &b.contasPagar);
 
-    calcularBalanco(&balanco, &res);
-    calcularIndices(&balanco, &dre, &res);
+    // -------- PASSIVO NÃO CIRCULANTE --------
+    printf("\n--- ENTRADA BALANCO (PASSIVO NAO CIRCULANTE) ---\n");
+    printf("Emprestimos LP: "); scanf("%lf", &b.emprestimosLP);
+    printf("Provisoes: "); scanf("%lf", &b.provisoes);
+
+    // -------- PATRIMÔNIO LÍQUIDO --------
+    printf("\n--- ENTRADA BALANCO (PATRIMONIO LIQUIDO) ---\n");
+    printf("Capital Social: "); scanf("%lf", &b.capitalSocial);
+    printf("Reservas de Capital: "); scanf("%lf", &b.reservasCapital);
+    printf("Reservas de Lucros: "); scanf("%lf", &b.reservasLucros);
+    printf("Lucros Acumulados (Inicial): "); scanf("%lf", &b.lucrosAcumulados);
+
+    calcularBalanco(&b, r.lucroLiquido);
+    calcularIndices(&b, &d, &r);
 
     return 0;
 }
